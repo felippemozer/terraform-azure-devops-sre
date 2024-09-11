7faa2cb75ad170effed58c661eb5b7eb53ddfe41
felippemozer <felippemozer22@gmail.com>
2024-09-11 11:04:04 -0300

    feat: section 22

--- 22-terraform-backend/locals.tf
+++ 22-terraform-backend/locals.tf
@@ -1,7 +1,7 @@
 locals {
   owners               = var.business_division
   environment          = var.environment
-  resource_name_prefix = "${var.business_division}-${var.environment}"
+  resource_name_prefix = "${var.resource_group_location}-${var.business_division}-${var.environment}"
   common_tags = {
     owners      = local.owners
     environment = local.environment
